37a02d83219fd2385e98bfaae7da75474b419eae
atomazos3 <antonis.pia@gmail.com>
2025-06-25 10:33:29 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
-// Replace with your Firebase config
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MSG_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAsg2lL30ZeVSaRIUhaIyMI0bAEZ7LqL5w",
+  authDomain: "aat-tasks.firebaseapp.com",
+  projectId: "aat-tasks",
+  storageBucket: "aat-tasks.firebasestorage.app",
+  messagingSenderId: "242226990759",
+  appId: "1:242226990759:web:42a53b6871e11dc2435582",
+  measurementId: "G-D1WMQC728R"
 };
 
 const app = initializeApp(firebaseConfig);
